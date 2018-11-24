8bdf0129060b20a939548f9bd8cc4881a9692c3c
lreg <lreg0@web.de>
2018-11-24 14:09:01 +0100

    Fehlerhafte Entwicklung rückgängig gemacht

--- DatensammlungEclipse/src/Information.java
+++ DatensammlungEclipse/src/Information.java
@@ -5,17 +5,14 @@ public class Information {
 	private String[] text;
 
 	public Information(Category c, String[] text) {
-		/*
-		 * enter the Category to the object and use its information for the lenght of
-		 * text
-		 */
+		//enter the Category to the object and use its information for the lenght of text
 		this.category = c;
 		this.text = new String[category.getLenght() + 1];
 		this.text[0] = category.getName();
 	}
 
 	public void setText(String[] text) {
-		this.text = text;
+		//TODO add text correctly in the Strings Text
 	}
 
 	public String[] getText() {
